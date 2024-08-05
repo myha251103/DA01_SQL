353d679937614f0a8f36b068a91cc43c67791fe6
myha251103 <hahoanmy251103@gmail.com>
2024-08-05 11:43:06 -0500

    Update Day 11_practice.sql

--- Day 11_practice.sql
+++ Day 11_practice.sql
@@ -3,3 +3,16 @@ select b.continent, floor(avg(a.population))
 from city as a
 inner join country as b on a.COUNTRYCODE = b.CODE
 group by b.CONTINENT;
+---bt2
+select round(cast(count(t2.email_id)as decimal)/count(t1.email_id),2) as activation_rate
+from emails as t1
+left join texts as t2 on t1.email_id = t2.email_id 
+and t2.signup_action = 'Confirmed';
+
+vì sao bt2 version bên dưới đây lại có kq bằng 0?
+select round(cast(count(t2.email_id)/count(t1.email_id) as decimal),2) as activation_rate
+from emails as t1
+left join texts as t2 on t1.email_id = t2.email_id 
+and t2.signup_action = 'Confirmed';
+
+---bt3
